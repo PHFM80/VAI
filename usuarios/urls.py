cb9f443681a48f8892c3116235019fa849909f0c
PHFM80 <pablofloresmaza@gmail.com>
2024-08-03 18:24:29 -0300

    agregado de carpeta de imagenes de home.  terminacion del login sin persistir ni migrar. creacion del dashboard sin estilos

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -5,8 +5,8 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
-    # path('dashboard/', views.user_dashboard, name= 'dashboard'), 
-    # path('logout/', auth_views.LogoutView.as_view(), name= 'logout'),
-    # path('register/', views.user_register, name='register'),
+    path('dashboard/', views.dashboard_view, name= 'dashboard'), 
+    path('logout/', auth_views.LogoutView.as_view(), name= 'logout'),
+    path('register/', views.register_view, name='register'),
 
 ]
